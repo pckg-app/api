90e00178c51b59255844a206dfb3f89cfa3f3771
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-12-29 23:42:06 +0100

    Relation

--- src/Pckg/Api/Entity/AppKeys.php
+++ src/Pckg/Api/Entity/AppKeys.php
@@ -2,10 +2,17 @@
 
 use Pckg\Api\Record\AppKey;
 use Pckg\Database\Entity;
+use Pckg\Mailo\Entity\Apps;
 
 class AppKeys extends Entity
 {
 
     protected $record = AppKey::class;
+
+    public function app()
+    {
+        return $this->belongsTo(Apps::class)
+                    ->foreignKey('app_id');
+    }
 
 }
